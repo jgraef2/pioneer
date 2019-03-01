45af678880d6bd9207b68dfc4a14ee21db12c0b8
jgraef2 <jgraef3@kent.edu>
2019-03-01 14:31:39 -0800

    NEW NAV HAMBURGER WORKING

--- nav/js/index.js
+++ nav/js/index.js
@@ -1,16 +1,16 @@
 var sidebarBox = document.querySelector('#box'),
-sidebarBtn = document.querySelector('#btn'),
+sidebarbtn = document.querySelector('#btn'),
 pageWrapper = document.querySelector('#page-wrapper');
 
-sidebarBtn.addEventListener('click', function (event) {
-		sidebarBtn.classList.toggle('active');
+sidebarbtn.addEventListener('click', function (event) {
+		sidebarbtn.classList.toggle('active');
 		sidebarBox.classList.toggle('active');
 });
 
 pageWrapper.addEventListener('click', function (event) {
 
 		if (sidebarBox.classList.contains('active')) {
-				sidebarBtn.classList.remove('active');
+				sidebarbtn.classList.remove('active');
 				sidebarBox.classList.remove('active');
 		}
 });
@@ -18,7 +18,7 @@ pageWrapper.addEventListener('click', function (event) {
 window.addEventListener('keydown', function (event) {
 
 		if (sidebarBox.classList.contains('active') && event.keyCode === 27) {
-				sidebarBtn.classList.remove('active');
+				sidebarbtn.classList.remove('active');
 				sidebarBox.classList.remove('active');
 		}
 });
