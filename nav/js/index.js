8c2fd06307b438586a72e5e0d1403049c2a49f68
jgraef2 <jgraef3@kent.edu>
2019-03-01 16:36:11 -0800

    javascript edit

--- nav/js/index.js
+++ nav/js/index.js
@@ -2,6 +2,7 @@ var sidebarBox = document.querySelector('#box'),
 sidebarbtn = document.querySelector('#btn'),
 pageWrapper = document.querySelector('#page-wrapper');
 
+window.onload=function(){
 sidebarbtn.addEventListener('click', function (event) {
 		sidebarbtn.classList.toggle('active');
 		sidebarBox.classList.toggle('active');
@@ -14,6 +15,7 @@ pageWrapper.addEventListener('click', function (event) {
 				sidebarBox.classList.remove('active');
 		}
 });
+}
 
 window.addEventListener('keydown', function (event) {
 
