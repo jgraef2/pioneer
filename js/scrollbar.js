9693a31658c32836dfde6395a410d47c0997589f
jgraef2 <jgraef3@kent.edu>
2019-05-09 11:36:29 -0700

    Big Push
    Final contact/what we do/who we are (I think) haha.

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,9 +1,26 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+var scrolling = false;
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
+$( window ).scroll( function() {
+  scrolling = true;
+});
+
+setInterval( function() {
+  if ( scrolling ) {
+    scrolling = false;
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+  }
+}, 250 );
+
+
+// When the user scrolls the page, execute myFunction
+//window.onscroll = function() {myFunction()};
+
+//function myFunction() {
+//  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+//  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//  var scrolled = (winScroll / height) * 100;
+//  document.getElementById("myBar").style.width = scrolled + "%";
+//}
