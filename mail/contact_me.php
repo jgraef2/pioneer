3b55612d55adec6a1bb0b38dd028bb9daceafe09
jgraef2 <jgraef3@kent.edu>
2019-09-27 17:39:06 -0700

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,7 +1,6 @@
 <?php
 // Check for empty fields
-if(empty($_POST['email']) 		||
-   empty($_POST['message'])	||
+if(empty($_POST['email'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
@@ -9,12 +8,11 @@ if(empty($_POST['email']) 		||
    }
 
 $email_address = $_POST['email'];
-$message = $_POST['message'];
 
 // Create the email and send the message
 $to = 'josh@pioneermade.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Pioneer Application Via FormSpree";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nEmail: $email_address\n\nMessage:\n$message";
+$email_subject = "Warm Welcome Request";
+$email_body = "You have a new request for a warm welcome.\nEmail: $email_address\n";
 $headers = "From: noreply@pioneermade.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
