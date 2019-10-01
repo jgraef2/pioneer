e85c88d8ed34cccd66b844e6b1df407b7b3679e8
jgraef2 <jgraef3@kent.edu>
2019-09-30 21:57:57 -0700

    js issues

--- js/SVG_EFFECTS.js
+++ js/SVG_EFFECTS.js
@@ -9,13 +9,10 @@ window.onload=function() {
 	var svgDoc = a.contentDocument;
 	// Get one of the SVG items by ID;
 	var svgItem = svgDoc.getElementById("Mask_Group_201");
-  window.alert("4");
 	// Set the colour to something else
 	svgItem.setAttribute("fill", "#5D0000");
-  window.alert("5");
 
   sendbtn2.addEventListener('mouseover', function(){
-    window.alert("moused over");
   svgItem.setAttribute("fill", "#F9423A");
   })
   sendbtn2.addEventListener('mouseout', function(){
