388fcd9c9968a3f4522206c95165996c628381b1
jgraef2 <jgraef3@kent.edu>
2019-09-30 21:39:20 -0700

    dasdf

--- js/SVG_EFFECTS.js
+++ js/SVG_EFFECTS.js
@@ -1,33 +1,25 @@
 
-window.alert("JS connected");
-
 window.onload=function() {
-  window.alert("1");
+
+  var sendbtn2 = document.getElementById("sendbtn2");
 
 	// Get the Object by ID
 	var a = document.getElementById("buttonbglines");
-  window.alert("2");
 	// Get the SVG document inside the Object tag
 	var svgDoc = a.contentDocument;
-  window.alert("3");
 	// Get one of the SVG items by ID;
-	var svgItem = svgDoc.getElementById("buttonbglines");
+	var svgItem = svgDoc.getElementById("Mask_Group_201");
   window.alert("4");
 	// Set the colour to something else
-	svgItem.setAttribute("fill", "lime");
+	svgItem.setAttribute("fill", "#5D0000");
   window.alert("5");
+
+  sendbtn2.addEventListener('mouseover', function(){
+    window.alert("moused over");
+  svgItem.setAttribute("fill", "#F9423A");
+  })
+  sendbtn2.addEventListener('mouseout', function(){
+  svgItem.setAttribute("fill", "#5D0000");
+  })
+
 };
-
-var buttonbglines = document.getElementById("buttonbglines").contentDocument;
-var sendbtn2 = document.getElementById("sendbtn2");
-
-var fill = parseFloat(element.getAttributeNS(null, 'fill'));
-element.setAttributeNS(null, 'fill', yellow);
-
-sendbtn2.addEventListener('mouseover', function(){
-  window.alert("moused over");
-buttonbglines.style.fill = 'yellow';
-})
-sendbtn2.addEventListener('mouseout', function(){
-buttonbglines.style.backgroundColor = 'yellow';
-})
