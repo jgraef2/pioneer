e55cb7906b8c5e30cdd90c6618f82666095aa40a
jgraef2 <jgraef3@kent.edu>
2019-09-30 21:17:20 -0700

    test

--- js/SVG_EFFECTS.js
+++ js/SVG_EFFECTS.js
@@ -1,20 +1,33 @@
-$(document).ready(function() {
-var buttonbglines = document.getElementById("buttonbglines");
-var sendbtn2 = document.getElementById("sendbtn2")
 
-// It's important to add an load event listener to the object,
-// as it will load the svg doc asynchronously
-buttonbglines.addEventListener("load",function(){
-  // get the inner DOM of image
-  var svgDoc = buttonbglines.contentDocument;
-  // get the inner element by id (in my svg I have a path i need to change with
-  //id='Hat')
-  var area = svgDoc.getElementById("buttonbglines");
-  // add behaviour
-  sendbtn2.addEventListener("mouseover",function(){
-   area.style.fill = '#F9423A'
-  });
-  sendbtn2.addEventListener('mouseout', function(){
-   area.style.fill = '#5D0000'
-  })
-});
+window.alert("JS connected");
+
+window.onload=function() {
+  window.alert("1");
+
+	// Get the Object by ID
+	var a = document.getElementById("buttonbglines");
+  window.alert("2");
+	// Get the SVG document inside the Object tag
+	var svgDoc = a.contentDocument;
+  window.alert("3");
+	// Get one of the SVG items by ID;
+	var svgItem = svgDoc.getElementById("poop");
+  window.alert("4");
+	// Set the colour to something else
+	svgItem.setAttribute("fill", "lime");
+  window.alert("5");
+};
+
+var buttonbglines = document.getElementById("buttonbglines").contentDocument;
+var sendbtn2 = document.getElementById("sendbtn2");
+
+var fill = parseFloat(element.getAttributeNS(null, 'fill'));
+element.setAttributeNS(null, 'fill', yellow);
+
+sendbtn2.addEventListener('mouseover', function(){
+  window.alert("moused over");
+buttonbglines.style.fill = 'yellow';
+})
+sendbtn2.addEventListener('mouseout', function(){
+buttonbglines.style.backgroundColor = 'yellow';
+})
